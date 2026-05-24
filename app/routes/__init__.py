25586beafed5661564728ee24ad88cd69298134a
mapasariza09-colaboracion <mapasariza09@gmail.com>
2026-05-24 10:58:56 -0500

    fix database

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -6,15 +6,21 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'amapola_secret_key'
-
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     login_manager.init_app(app)
 
+    # IMPORTAR MODELOS
+    from app.models import User, Producto, Order
+
+    # IMPORTAR RUTAS
     from app.routes.auth.routes import auth
     app.register_blueprint(auth)
+
+    # CREAR TABLAS
+    with app.app_context():
+        db.create_all()
 
     return app
