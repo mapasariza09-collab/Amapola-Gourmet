dafefe6ec83e5350a841743ec5e072095f9645d8
mapasariza09-colaboracion <mapasariza09@gmail.com>
2026-05-23 20:18:20 -0500

    configure flask app

---
+++ app/routes/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from app.extensions import db, login_manager
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'amapola_secret_key'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app.routes.auth.routes import auth
+    app.register_blueprint(auth)
+
+    return app
