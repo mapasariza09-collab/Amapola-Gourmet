9d40b19dd05209ccca196c2bb56e0d582c77903d
mapasariza09-colaboracion <mapasariza09@gmail.com>
2026-05-24 19:26:44 -0500

    tablas

--- app/run.py
+++ app/run.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from app.extensions import db, login_manager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'amapola123'
-import os
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 login_manager.init_app(app)
+
+# IMPORTAR MODELOS
+from app.models import User, Producto, Order
+
+# CREAR TABLAS
+with app.app_context():
+    db.create_all()
 
 # IMPORTAR BLUEPRINT
 from app.routes.auth.routes import auth
